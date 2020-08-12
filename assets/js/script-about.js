9c420f0386d9dba58a711e4245ffa7a30e249e85
Dan Høegh <dan.hoegh@gmail.com>
2020-08-12 10:57:08 +0200

    done

---
+++ assets/js/script-about.js
@@ -0,0 +1,8 @@
+// JS by Dan Høegh
+// UCN MMD 2020
+
+// ABOUT PAGE
+
+videojs('video3', background);
+
+videojs('video4', content);
---
+++ assets/js/script-home.js
@@ -0,0 +1,24 @@
+// JS by Dan Høegh
+// UCN MMD 2020
+
+// // No options are set, default options will be used
+// videojs('video2', null); 
+
+
+// // Setting options in a json object
+// let options = {
+//     "controls": false, 
+//     "autoplay": true, 
+//     "preload": "auto", 
+//     "muted": true
+// };
+
+// // try the different json-objects here
+// videojs('video2', options);
+
+// // If you want to trigger some code when the player is ready
+// videojs('video2').on('ready', function() {
+//   this.addClass('my-example'); 
+//   // adds the class name "my-example" when videojs is loaded and ready
+// });
+
