ff83ae636fa4b1a50c1c831b1e9e0b035454c242
Dan Høegh <dan.hoegh@gmail.com>
2020-08-28 11:38:40 +0200

    added code to lesson version, to reduce code time in lecture

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
 // JS by Dan Høegh
 // UCN MMD 2020
 
+let background = {
+    "controls": false, 
+    "autoplay": true, 
+    "preload": "auto", 
+    "muted": true,
+    "loop": true
+};
+
+// content should: 
+// not autoplay
+// have sound on
+// preload
+// not loop
+// should show controls
+let content = {
+    
+};
